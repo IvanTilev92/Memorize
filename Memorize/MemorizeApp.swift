03b5a46f3c6e4a73953a8911d8e22081ce4c40b4
Ivan Tilev <iivantilev@gmail.com>
2021-09-08 23:29:53 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ivan Tilev on 8.09.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
